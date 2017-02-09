f1cf9f053cdf714be1281ec70c6f3ebf5d414342
Juan Manuel Jimenez Sanchez <juanmjimenezs@gmail.com>
2017-02-08 20:22:33 -0500

    Se agregaron los archivos .swift necesarios y se vincularon con el storyboard

---
+++ queRule/GameCell.swift
@@ -0,0 +1,13 @@
+//
+//  GameCell.swift
+//  queRule
+//
+//  Created by Juan Manuel Jimenez Sanchez on 8/02/17.
+//  Copyright © 2017 Juan Manuel Jimenez Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class GameCell: UICollectionViewCell {
+    
+}
